35c72a8d45d44c0886bff4df3fe574450f3cc06b
F.thawin <thawin.fuf@gmail.com>
2021-06-09 14:13:33 +0700

    Create Exercise4
    Added Variable, Score & command"Print"

---
+++ Exercise4_Thawin_F.py
@@ -0,0 +1,13 @@
+FE = 90.2
+GE = 80.7
+ICS = 95.6
+CP = 89.2
+print("         --- Your Score ---","\n"
+
+"Foundation English               : ",FE,"\n"
+
+"General Business                 : ",GE,"\n"
+      
+"Introduction to Computer Systems : ",ICS,"\n"
+      
+"Computer Programming             : ",CP)
